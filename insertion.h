651ad500a9449d6c7bafbac91748efa53b32c4ac
yz723 <yz723@duke.edu>
2023-04-01 22:39:43 -0400

    update insertion.h file

--- insertion.h
+++ insertion.h
@@ -4,12 +4,14 @@ using namespace pqxx;
 #ifndef _QUERY_FUNCS_
 #define _QUERY_FUNCS_
 
-void addAccount(connection *C, int account_id, double balance, int position_id);
-void addPosition(connection *C, string symbol, int shares);
+void addAccount(connection *C, int account_id, double balance);
+void updateAccount(connection *C, int account_id, double balance);
+void addPosition(connection *C, string symbol, int account_id, int shares);
+void updatePosition(connection *C, string symbol, int account_id, int shares);
 void addOpenOrder(connection *C, int transaction_id, int shares, double limit_price, string symbol);
 void addExecuteOrder(connection *C, int transaction_id, int shares, std::time_t time, double execute_price);
 void addCancelOrder(connection *C, int transaction_id, int shares, std::time_t time);
 void deleteOpenOrder(connection *C, int open_id);
 void updateOpenOrder(connection *C, int open_id, int shares);
-bool checkPosition(connection *C, string symbol);
+
 #endif
