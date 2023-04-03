53e3da9fe38142da53bf7b63c4e4636bc0251d32
yz723 <yz723@duke.edu>
2023-04-03 00:40:27 -0400

    update client.cpp and server.cpp; client and server can communicate XML

--- client.cpp
+++ client.cpp
@@ -6,8 +6,47 @@
 #include <arpa/inet.h>
 #include <vector>
 #include <fstream>
+#include <string>
 
 
+void communicateXML(int sockfd, std::string directory) {
+    // send XML file to server
+    char buffer[1024] = {0};
+    std::vector<std::string> filenames = {"createAccount1.xml", "createAccount2.xml"};
+    for (const auto& filename : filenames) {
+        std::ifstream file("./"+directory+ "/" + filename);
+        if (file.is_open()) {
+            std::string fileContent;
+            std::string line;
+            while (std::getline(file, line)) {
+                fileContent += line;
+            }
+            file.close();
+            const char* data = fileContent.c_str();
+            send(sockfd, data, strlen(data), 0);
+            int bytesReceived = recv(sockfd, buffer, 1024, 0);
+            if (bytesReceived < 0) {
+                std::cerr << "Failed to receive data" << std::endl;
+            } else {
+                buffer[bytesReceived] = '\0';
+                std::string response = buffer;
+                std::cout << "Received response: " << response << std::endl;
+                // store response in file
+                std::ofstream outfile("clientResult.xml");
+                if (outfile.is_open()) {
+                    outfile << response;
+                    outfile.close();
+                    std::cout << "Response stored in clientResult.xml" << std::endl;
+                } else {
+                    std::cerr << "Failed to open file for writing" << std::endl;
+                }
+            }
+            memset(buffer, 0, sizeof(buffer));
+        } else {
+            std::cerr << "Failed to open file: " << filename << std::endl;
+        }
+    }
+}
 
 int main()
 {
@@ -16,8 +55,7 @@ int main()
 
     int sockfd;
     struct addrinfo hints, *host_info_list, *p;
-    char buffer[1024] = {0};
-
+    
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6, whichever is available
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
@@ -47,27 +85,9 @@ int main()
 
     freeaddrinfo(host_info_list);
  
-    // send XML file to server
-    std::vector<std::string> filenames = {"createAccount1.xml", "createAccount2.xml"};
-    std::string file_contents;
-    for (const auto& filename : filenames) {
-        std::ifstream file("./data/" + filename);
-        if (file.is_open()) {
-            std::string line;
-            while (std::getline(file, line)) {
-                file_contents += line;
-            }
-            file.close();
-            const char* data = file_contents.c_str();
-            send(sockfd, data, strlen(data), 0);
-
-            int valread = read(sockfd, buffer, 1024);
-            std::cout << buffer << std::endl;
-        } else {
-            std::cerr << "Failed to open file: " << filename << std::endl;
-        }
-    }
-
+    // send a message to the server
+    std::string directory = "data";
+    communicateXML(sockfd, directory);
     // use sockfd for further communication with the server
     close(sockfd);
 
